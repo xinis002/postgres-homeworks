553fab3c27605db88ed0e8fc2c08aa46c493d191
xinis002 <158053952+xinis002@users.noreply.github.com>
2024-03-30 00:59:01 +0300

    Update filter_sorting.sql

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -10,6 +10,7 @@ ORDER BY ship_country, ship_city;
 SELECT AVG (shipped_date - order_date) as total  FROM orders where ship_country='Germany';
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
+SELECT MIN(unit_price), MAX(unit_price) FROM products WHERE discontinued IS NOT NULL; 
 
 
 -- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
