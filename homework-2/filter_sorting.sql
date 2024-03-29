cd985cc7525865856830c73ebcfa5fa2dd851152
xinis002 <158053952+xinis002@users.noreply.github.com>
2024-03-30 00:51:01 +0300

    Update filter_sorting.sql

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -7,7 +7,7 @@ SELECT DISTINCT ship_country, ship_city FROM orders
 ORDER BY ship_country, ship_city;
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-
+SELECT AVG (shipped_date - order_date) as total  FROM orders where ship_country='Germany';
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
 
