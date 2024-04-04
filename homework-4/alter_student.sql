eecad2316220999ccea6adb573000b5b5592c2b5
xinis002 <158053952+xinis002@users.noreply.github.com>
2024-04-04 21:37:03 +0300

    Update alter_student.sql

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -20,7 +20,8 @@ ALTER TABLE student DROP COLUMN middle_name;
 ALTER TABLE student RENAME birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student 
+ALTER COLUMN phone SET DATA TYPE varchar(32)
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
 
